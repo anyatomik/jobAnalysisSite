3dc54c2bf0b55fcc3f76650040b9f5d52f990471
anyatomik <anya.tomilova15@gmail.com>
2026-03-08 22:31:14 +0500

    add basic table

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #здесь ссылка на бд
+database = SQLAlchemy(app)
+
+
+class vacancy(database.Model):
+    searchGeo = database.Column(database.String)
+    salaryRan = database.Coulumn(database.String)
+    wrkExp = database.Column(database.String)
+    wrkType = database.Column(database.String)
+    company = database.Column(database.String)
+
+'''
+
+
+@app.route('/')
+def mainn():
+    #vacantions = vacancy.query.all()
+    return render_template('index.html') #vacantions = vacantions
+
+if __name__ == '__main__':
+    app.run(debug=True)
