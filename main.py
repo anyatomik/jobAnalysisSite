ab44f5fa2a74b08140bb36f5494c60dbac5062de
anyatomik <anya.tomilova15@gmail.com>
2026-03-09 20:04:46 +0500

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask,render_template
+from flask_frozen import Freezer
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app = Flask(__name__)
+
+freezer = Freezer(app)
 '''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #здесь ссылка на бд
 database = SQLAlchemy(app)
@@ -19,8 +24,10 @@ class vacancy(database.Model):
 
 @app.route('/')
 def mainn():
+
     #vacantions = vacancy.query.all()
     return render_template('index.html') #vacantions = vacantions
 
 if __name__ == '__main__':
+    freezer.freeze()
     app.run(debug=True)
